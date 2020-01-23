8bdd04a0d1119c900f3460c1b8444e0d7b5a1e47
Petrit Krasniqi <Petrit.Krasniqi@students.ittralee.ie>
2020-01-23 11:05:23 +0000

    Selecting and deselecting object

---
+++ Assets/Controlable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlable : MonoBehaviour
+{
+
+    public bool isSelected = false;
+
+    Renderer my_renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    internal void move_up()
+    {
+        if (isSelected)
+        {
+            transform.position += Vector3.up;
+            my_renderer.material.color = Color.red;
+        } 
+        
+    }
+}
---
+++ Assets/TouchControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+
+    Camera my_camera;
+    Controlable currently_selected_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray my_ray = my_camera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(my_ray.origin, 20 * my_ray.direction);
+
+        RaycastHit info_on_hit;
+
+        if (Physics.Raycast(my_ray, out info_on_hit))
+        {
+            Controlable my_object = info_on_hit.transform.GetComponent<Controlable>();
+            if (currently_selected_object)
+            {
+                if (my_object != currently_selected_object)
+                {
+                    currently_selected_object.deselect();
+                    my_object.isSelected();
+                    currently_selected_object = my_object;
+                }
+            }
+            else
+            {
+                my_object.isSelected();
+                currently_selected_object = my_object;
+            }
+
+            my_object.isSelected = true;
+            my_object.move_up();
+        }
+        }
+    }
+}
